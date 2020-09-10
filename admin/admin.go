23bdde83c493e1dc2e1535212b3a18095d19a74c
BlazeOxin <nomaanakhtar643@gmail.com>
2020-09-11 02:21:37 +0500

    Fixed Core Property Mapping Algorithm

--- admin/admin.go
+++ admin/admin.go
@@ -3,8 +3,10 @@ package admin
 import (
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/fatih/structs"
+	"github.com/fatih/structtag"
 	"github.com/gofiber/fiber"
 	"github.com/gosimple/slug"
 	"gorm.io/gorm"
@@ -14,17 +16,17 @@ import (
 
  */
 type Model struct {
-	Name   string
-	Object interface{}
+	ObjectPtr interface{}
+	Fields    map[string]Field
 }
 
 /*Field :
 
  */
 type Field struct {
-	Type    string
-	Select  bool
-	Choices []string
+	Type            string
+	Choices         []string
+	ForeignKeyModel string
 }
 
 /*FieldWithValue :
@@ -37,10 +39,8 @@ type FieldWithValue struct {
 	Value   interface{}
 }
 
-/*ContentManager :
-
- */
-var ContentManager = make(map[string][]Model)
+var lModels = make(map[string]Model)
+var lApps = make(map[string][]string)
 
 func getStructName(structure interface{}) string {
 	valueOf := reflect.ValueOf(structure)
@@ -51,6 +51,14 @@ func getStructName(structure interface{}) string {
 	return valueOf.Type().Name()
 }
 
+func getFieldType(field reflect.StructField) string {
+	fType := strings.Replace(fmt.Sprintf("%s", field.Type), "[]", "", -1)
+	if strings.Contains(fType, ".") {
+		return strings.Split(fType, ".")[1]
+	}
+	return fType
+}
+
 func getStructNameList(structList []interface{}) []string {
 	var output []string
 	for _, v := range structList {
@@ -58,81 +66,142 @@ func getStructNameList(structList []interface{}) []string {
 	}
 	return output
 }
-func createStructFieldMap(inputStruct interface{}) map[string]interface{} {
-	var output map[string]interface{}
 
-	for _, fieldName := range structs.Names(inputStruct) {
-		valueOf := reflect.ValueOf(inputStruct)
-		field := valueOf.FieldByName(fieldName)
+// func createStructFieldMap(inputStruct interface{}) map[string]interface{} {
+// 	var output map[string]interface{}
 
-		fieldSelect := false
-		fieldType := "text"
+// 	for _, fieldName := range structs.Names(inputStruct) {
+// 		valueOf := reflect.ValueOf(inputStruct)
+// 		field := valueOf.FieldByName(fieldName)
 
-		switch field.Kind() {
-		case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint32, reflect.Uint64:
-			fieldType = "number"
-			break
-		case reflect.String:
-			fieldType = "text"
-			break
-		case reflect.Bool:
-			fieldType = "checkbox"
-			break
-		case reflect.Struct:
-			fieldType = "foreignkey"
-			fieldSelect = true
-			break
+// 		fieldSelect := false
+// 		fieldType := "text"
+
+// 		switch field.Kind() {
+// 		case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint32, reflect.Uint64:
+// 			fieldType = "number"
+// 			break
+// 		case reflect.String:
+// 			fieldType = "text"
+// 			break
+// 		case reflect.Bool:
+// 			fieldType = "checkbox"
+// 			break
+// 		case reflect.Struct:
+// 			fieldType = "foreignkey"
+// 			fieldSelect = true
+// 			break
+// 		}
+// 		output[fieldName] = Field{
+// 			Select: fieldSelect,
+// 			Type:   fieldType,
+// 		}
+// 	}
+
+// 	return output
+// }
+
+// func createStructFieldWithValueMap(inputStruct interface{}) map[string]interface{} {
+// 	var output map[string]interface{}
+
+// 	for _, fieldName := range structs.Names(inputStruct) {
+// 		valueOf := reflect.Indirect(reflect.ValueOf(inputStruct))
+// 		field := valueOf.FieldByName(fieldName)
+
+// 		fieldSelect := false
+// 		fieldType := "text"
+// 		var fieldValue interface{}
+
+// 		switch field.Kind() {
+// 		case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint32, reflect.Uint64:
+// 			fieldType = "number"
+// 			fieldValue = field
+// 			break
+// 		case reflect.String:
+// 			fieldType = "text"
+// 			fieldValue = field
+// 			break
+// 		case reflect.Bool:
+// 			fieldType = "checkbox"
+// 			fieldValue = field
+// 			break
+// 		case reflect.Struct:
+// 			fieldValue = createStructFieldWithValueMap(field)
+// 			fieldSelect = true
+// 			fieldType = "foreignkey"
+// 			break
+// 		default:
+// 			fieldValue = field
+// 			break
+// 		}
+// 		output[fieldName] = FieldWithValue{
+// 			Select: fieldSelect,
+// 			Value:  fieldValue,
+// 			Type:   fieldType,
+// 		}
+// 	}
+
+// 	return output
+// }
+
+func processFields(Model interface{}) map[string]Field {
+	Output := make(map[string]Field)
+	ModelValue := reflect.Indirect(reflect.ValueOf(Model))
+
+	for _, fieldName := range structs.Names(Model) {
+		fieldByValue := ModelValue.FieldByName(fieldName)
+		fieldByType, _ := ModelValue.Type().FieldByName(fieldName)
+
+		var fieldType string
+		var foreignKeyModel string
+
+		tags, _ := structtag.Parse(string(fieldByType.Tag))
+
+		gormTag, _ := tags.Get("gorm")
+		if gormTag != nil {
+			switch gormTag.Name {
+			case "primaryKey":
+				fieldType = "primarykey"
+			case "ForeignKey":
+				fieldType = "foreginkey"
+			}
+			if gormTag.Name == "primaryKey" {
+				fieldType = "primarykey"
+			} else if strings.Contains(gormTag.Name, "ForeignKey") {
+				fieldType = "foreignkey"
+				foreignKeyModel = getFieldType(fieldByType)
+			}
 		}
-		output[fieldName] = Field{
-			Select: fieldSelect,
-			Type:   fieldType,
+
+		adminTag, _ := tags.Get("admin")
+		if adminTag != nil {
+			switch adminTag.Name {
+			case "textarea":
+				fieldType = "textarea"
+			}
+		}
+
+		if fieldType == "" {
+			switch fieldByValue.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Float32, reflect.Float64:
+				fieldType = "number"
+			case reflect.String:
+				fieldType = "text"
+			case reflect.Array:
+				fieldType = "manytomany"
+				foreignKeyModel = getFieldType(fieldByType)
+			case reflect.Bool:
+				fieldType = "checkbox"
+			}
+		}
+
+		Output[fieldName] = Field{
+			Type:            fieldType,
+			ForeignKeyModel: foreignKeyModel,
 		}
 	}
 
-	return output
-}
-
-func createStructFieldWithValueMap(inputStruct interface{}) map[string]interface{} {
-	var output map[string]interface{}
-
-	for _, fieldName := range structs.Names(inputStruct) {
-		valueOf := reflect.Indirect(reflect.ValueOf(inputStruct))
-		field := valueOf.FieldByName(fieldName)
-
-		fieldSelect := false
-		fieldType := "text"
-		var fieldValue interface{}
-
-		switch field.Kind() {
-		case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint32, reflect.Uint64:
-			fieldType = "number"
-			fieldValue = field
-			break
-		case reflect.String:
-			fieldType = "text"
-			fieldValue = field
-			break
-		case reflect.Bool:
-			fieldType = "checkbox"
-			fieldValue = field
-			break
-		case reflect.Struct:
-			fieldValue = createStructFieldWithValueMap(field)
-			fieldSelect = true
-			fieldType = "foreignkey"
-			break
-		default:
-			fieldValue = field
-			break
-		}
-		output[fieldName] = FieldWithValue{
-			Select: fieldSelect,
-			Value:  fieldValue,
-			Type:   fieldType,
-		}
-	}
-
-	return output
+	return Output
 }
 
 /*AddSection :
@@ -140,15 +209,18 @@ register your Database Structs into different sections
 effictively mimic Django Admin App based Object Grouping
 */
 func AddSection(name string, inputStructs ...interface{}) {
-	var sectionStructs []Model
+	ModelList := []string{}
 	for _, iterStruct := range inputStructs {
-
-		sectionStructs = append(sectionStructs, Model{
-			Name:   getStructName(iterStruct),
-			Object: iterStruct,
-		})
+		modelName := getStructName(iterStruct)
+		lModels[modelName] = Model{
+			ObjectPtr: iterStruct,
+			Fields:    processFields(iterStruct),
+		}
+		ModelList = append(ModelList, modelName)
 	}
-	ContentManager[name] = sectionStructs
+	lApps[name] = ModelList
+	fmt.Println(lApps)
+	fmt.Println(lModels)
 }
 
 /*SetupRoutes :
@@ -158,69 +230,63 @@ func SetupRoutes(app *fiber.App, db *gorm.DB) {
 	app.Get("/admin", func(c *fiber.Ctx) {
 		c.Render("admin/home", fiber.Map{
 			"Title":                 "Admin Site",
-			"ContentManager":        ContentManager,
+			"Apps":                  lApps,
 			"Slugify":               slug.Make,
-			"ShowContentManagement": len(ContentManager) > 0,
+			"ShowContentManagement": len(lApps) > 0,
 		}, "layout/admin")
 	})
 
-	for sectionName, contentList := range ContentManager {
-		app.Get(fmt.Sprintf("/admin/%s", slug.Make(sectionName)), func(c *fiber.Ctx) {
+	for AppName, ContentList := range lApps {
+		app.Get(fmt.Sprintf("/admin/%s", slug.Make(AppName)), func(c *fiber.Ctx) {
 			c.Render("admin/template/section", fiber.Map{
-				"Title":       sectionName,
-				"SectionName": sectionName,
-				"StructList":  contentList,
-				"Slugify":     slug.Make,
+				"Title":      AppName,
+				"AppName":    AppName,
+				"StructList": ContentList,
+				"Slugify":    slug.Make,
 			}, "layout/admin")
 		})
 
-		for _, contentStruct := range contentList {
+		for _, ModelName := range ContentList {
+			Model := lModels[ModelName]
+
 			var queriedData []interface{}
-			model := db.Model(contentStruct.Object)
-			model.Find(&queriedData)
+			DBModel := db.Model(Model.ObjectPtr)
+			DBModel.Find(&queriedData)
 			fmt.Print(queriedData)
 
-			app.Get(fmt.Sprintf("/admin/%s/%s", slug.Make(sectionName), slug.Make(contentStruct.Name)), func(c *fiber.Ctx) {
+			app.Get(fmt.Sprintf("/admin/%s/%s", slug.Make(AppName), slug.Make(ModelName)), func(c *fiber.Ctx) {
 				c.Render("admin/template/content", fiber.Map{
-					"Title":       contentStruct.Name,
-					"ContentName": contentStruct.Name,
-					"SectionName": sectionName,
-					"Fields":      structs.Names(contentStruct.Object),
-					"Items":       getStructNameList(queriedData),
-					"getValue": func(index int) interface{} {
-						return queriedData[index]
-					},
-					"Slugify": slug.Make,
+					"Title":     ModelName,
+					"AppName":   AppName,
+					"ModelName": ModelName,
+					"Fields":    lModels[ModelName],
+					"Slugify":   slug.Make,
 				}, "layout/admin")
 			})
 
-			app.Get(fmt.Sprintf("/admin/%s/%s/:id", slug.Make(sectionName), slug.Make(contentStruct.Name)), func(c *fiber.Ctx) {
+			app.Get(fmt.Sprintf("/admin/%s/%s/:id", slug.Make(AppName), slug.Make(ModelName)), func(c *fiber.Ctx) {
 				id := c.Params("id")
-				var contentObjectData interface{}
-
-				err := model.First(&contentObjectData, id).Error
-
+				err := DBModel.First(lModels[ModelName].ObjectPtr, id).Error
 				context := fiber.Map{
-					"Title":       fmt.Sprintf("Edit %s", contentStruct.Name),
-					"ContentName": contentStruct.Name,
-					"SectionName": sectionName,
-					"Slugify":     slug.Make,
-					"Data":        contentObjectData,
-					"HasError":    err != nil,
+					"Title":     fmt.Sprintf("Edit %s", ModelName),
+					"AppName":   AppName,
+					"ModelName": ModelName,
+					"Slugify":   slug.Make,
+					"Fields":    lModels[ModelName],
+					"HasError":  err != nil,
 				}
 				if err == nil {
 					context["ID"] = id
-					context["Fields"] = createStructFieldWithValueMap(contentStruct.Object)
 				}
 				c.Render("admin/template/edit", context, "layout/admin")
 			})
-			app.Get(fmt.Sprintf("/admin/%s/%s/create", slug.Make(sectionName), slug.Make(contentStruct.Name)), func(c *fiber.Ctx) {
+			app.Get(fmt.Sprintf("/admin/%s/%s/create", slug.Make(AppName), slug.Make(ModelName)), func(c *fiber.Ctx) {
 				c.Render("admin/template/create", fiber.Map{
-					"Title":       fmt.Sprintf("Create %s", contentStruct.Name),
-					"ContentName": contentStruct.Name,
-					"SectionName": sectionName,
-					"Slugify":     slug.Make,
-					"Fields":      structs.Names(contentStruct.Object),
+					"Title":     fmt.Sprintf("Create %s", ModelName),
+					"AppName":   AppName,
+					"ModelName": ModelName,
+					"Slugify":   slug.Make,
+					"Fields":    lModels[ModelName],
 				}, "layout/admin")
 			})
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/fatih/structs v1.1.0
+	github.com/fatih/structtag v1.2.0
 	github.com/gofiber/fiber v1.14.5
 	github.com/gofiber/template v1.5.5
 	github.com/gosimple/slug v1.9.0
